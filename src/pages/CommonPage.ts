81cf5c9ed318dcb13b1c8a4c190ce3910a4137bc
Flantoro <steculalviv@gmail.com>
2026-03-19 11:15:33 +0200

    added togglePasswordVisibility method

--- src/pages/CommonPage.ts
+++ src/pages/CommonPage.ts
@@ -1,10 +1,14 @@
 export default class CommonPage {
-    public get navigateBackIcon() {
-        return $('//com.horcrux.svg.SvgView'); 
-    }
+    public get navigateBackIcon() { return $('//com.horcrux.svg.SvgView'); }
+    public get showPasswordIcon() { return $('//android.widget.ImageView'); }
 
     public async clickNavigateBack() {
         await this.navigateBackIcon.waitForDisplayed();
         await this.navigateBackIcon.click();
+    }
+
+    public async togglePasswordVisibility() {
+        await this.showPasswordIcon.waitForDisplayed();
+        await this.showPasswordIcon.click();
     }
 }
