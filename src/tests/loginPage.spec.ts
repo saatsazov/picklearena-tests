6610c4755955f91f632bb2c5f1cc28813df1825d
Flantoro <steculalviv@gmail.com>
2026-03-20 11:45:58 +0200

    added new tests for login flow

--- src/tests/loginPage.spec.ts
+++ src/tests/loginPage.spec.ts
@@ -1,12 +1,41 @@
 import LoginPage from '../pages/LoginPage';
 import credentials from "../data/credentials";
+import data from "../data/data";
 
 describe('Authentication', () => {
-    it('should login successfully', async () => {
+
+    it('should show error when trying to login with empty credentials', async () => {
         await LoginPage.clickInitialLoginButton();
+        await LoginPage.clickSignInButton();
+
+        expect(await LoginPage.isMissingCredentialsErrorVisible()).toBe(true);
+        await LoginPage.clickNavigateBack();
+    });
+
+    it('should show error when trying to login with invalid credentials', async () => {
+        await LoginPage.clickInitialLoginButton();
+        await LoginPage.enterEmail(data.invalidEmail);
+        await LoginPage.enterPassword(data.invalidPassword);
+        await LoginPage.clickSignInButton();
+
+        expect(await LoginPage.isInvalidEmailPasswordErrorVisible()).toBe(true);
+        await LoginPage.clickNavigateBack();
+    });
+
+    it('should reset password', async () => {
+        await LoginPage.clickInitialLoginButton();
+        await LoginPage.clickForgotPasswordButton();
+        await LoginPage.enterRecoveryEmail(credentials.email);
+        await LoginPage.clickSendButton();
+        await LoginPage.clickGotItButton();
+        await LoginPage.clickNavigateBack();
+    });
+
+    it('should login successfully', async () => {
         await LoginPage.enterEmail(credentials.email);
         await LoginPage.enterPassword(credentials.password);
         await LoginPage.clickSignInButton();
-        await driver.pause(10000);
+
+        await driver.pause(5000);
     });
 });
